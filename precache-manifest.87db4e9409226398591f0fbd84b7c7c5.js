20055dc9970d878318dc3098ce9399454792f7ab
kumarnitinsingh <nitin962141@gmail.com>
2020-10-24 17:35:06 +0530

    Updates

---
+++ precache-manifest.87db4e9409226398591f0fbd84b7c7c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93e02e185facc9cc488306232e489e7",
+    "url": "/technews/index.html"
+  },
+  {
+    "revision": "b1f600022e83b0d4dab3",
+    "url": "/technews/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "75224374b13e41b9bb02",
+    "url": "/technews/static/css/main.aea78ef1.chunk.css"
+  },
+  {
+    "revision": "b1f600022e83b0d4dab3",
+    "url": "/technews/static/js/2.29644b5f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/technews/static/js/2.29644b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75224374b13e41b9bb02",
+    "url": "/technews/static/js/main.7743cc06.chunk.js"
+  },
+  {
+    "revision": "32bde34c863e06e77256",
+    "url": "/technews/static/js/runtime-main.3b36447f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/technews/static/media/SEGA.12a3db8b.woff"
+  }
+]);
